70708e080b0fc4f3361e172fd25a6f1c280a458a
Fazil <faziltirtana@tutanota.com>
2024-01-22 23:23:08 +0700

    prisma service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class TasksService {
+    constructor(private prismaService: PrismaService) { }
 }
