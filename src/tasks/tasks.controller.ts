23d4c882f7252a1041a0dec2b5815eb1d19a3f33
Fazil <faziltirtana@tutanota.com>
2024-01-22 23:30:32 +0700

    handlers

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
+
+    @Get()
+    async getTasks(@Query(ValidationPipe) filterDto: getTasksFilterDto): Promise<Task[]> {
+        if (Object.keys(filterDto).length) {
+            return await this.tasksService.getTasksWithFilters(filterDto);
+        }
+        return this.tasksService.getAllTasks();
+    }
+
+    @Get('/:id')
+    async getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return await this.tasksService.getTaskById(id);
+    }
+
+    @Delete('/:id')
+    async deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.tasksService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    async updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    ): Promise<Task> {
+        return await this.tasksService.updateTaskStatus(id, status);
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+        return await this.tasksService.createTask(createTaskDto);
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
     constructor(private prismaService: PrismaService) { }
+
+    async getAllTasks(): Promise<Task[]> {
+        const tasks: Task[] = await this.prismaService.task.findMany();
+
+        return tasks;
+    }
+
+    async getTasksWithFilters(filterDto: getTasksFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = this.prismaService.task.findMany({
+                where: {
+                    status: status,
+                }
+            });
+        }
+
+        if (search) {
+            tasks = this.prismaService.task.findMany({
+                where: {
+                    title: {
+                        contains: search,
+                    },
+                    description: {
+                        contains: search,
+                    }
+                }
+            });
+        }
+
+        return tasks;
+    }
+
+    async getTaskById(id: number): Promise<Task> {
+        const found = await this.prismaService.task.findUnique({
+            where: {
+                id: id,
+            }
+        })
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID ${id} not found.`);
+        }
+
+        return found;
+    }
+
+    async deleteTask(id: number): Promise<void> {
+        await this.prismaService.task.delete({
+            where: {
+                id: id,
+            }
+        });
+    }
+
+    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+        const task: Task = await this.prismaService.task.update({
+            where: {
+                id: id,
+            },
+            data: {
+                status: status,
+            }
+        });
+        return task;
+    }
+
+    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        const { title, description } = createTaskDto;
+
+        const task: Task = await this.prismaService.task.create({
+            data: {
+                title,
+                description,
+                status: TaskStatus.OPEN,
+            }
+        });
+
+        return ({
+            id: task.id,
+            title: task.title,
+            description: task.description,
+            status: task.status
+        });
+    }
 }
